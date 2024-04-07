41bdb1ede0024530128785f1bbca306c422bd1e1
manav-2004 <kushwahamanav71@gmail.com>
2024-04-07 22:54:24 +0530

    22:54 7/4  commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import PublishRide from './pages/PublishRide';
 
 function App() {
   return (
-    <LoginPage/>
+    // <>
+      <PublishRide/>
+    // </>
   );
 }
 
--- client/src/pages/SignUpPage.js
+++ client/src/pages/SignUpPage.js
@@ -1,47 +1,55 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-// import { Link ,useNavigate} from 'react-router-dom';
 
-const LoginPage = () => {
-    const [username, setUsername] = useState('');
+const SignUpPage = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleUsernameChange = (e) => {
-        setUsername(e.target.value);
-    };
-
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Add your login logic here
-        console.log('Username:', username);
+    const handleSubmit = () => {
+        // Handle sign-up logic here
+        console.log('Name:', name);
+        console.log('Email:', email);
         console.log('Password:', password);
     };
 
     return (
-        <section class = "">
+        <section>
             <div class="grid grid-cols-1 lg:grid-cols-2">
                 <div class="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-8">
                 <div class="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
                     <h2 class="text-3xl font-bold leading-tight text-black sm:text-4xl">
-                    Sign in
+                    Sign up
                     </h2>
-                    <p class="mt-2 text-sm text-gray-600">
-                    Don&#x27;t have an account?{" "}
+                    <p class="mt-2 text-base text-gray-600">
+                    Already have an account?{" "}
                     <Link
-                        to="/signup"
-                        class="font-semibold text-black transition-all duration-200 hover:underline"
+                        to="/login"
+                        class="font-medium text-black transition-all duration-200 hover:underline"
                     >
-                        Create a free account
+                        Sign In
                     </Link>
                     </p>
                     <form action="#" method="POST" class="mt-8">
                     <div class="space-y-5">
                         <div>
-                        <label for="" class="text-base font-medium text-gray-900">
+                        <label for="name" class="text-base font-medium text-gray-900">
+                            {" "}
+                            Full Name{" "}
+                        </label>
+                        <div class="mt-2">
+                            <input
+                            class="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                            type="text"
+                            placeholder="Full Name"
+                            id="name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            />
+                        </div>
+                        </div>
+                        <div>
+                        <label for="email" class="text-base font-medium text-gray-900">
                             {" "}
                             Email address{" "}
                         </label>
@@ -50,33 +58,30 @@ const LoginPage = () => {
                             class="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
                             type="email"
                             placeholder="Email"
-                            value={username}
-                            onChange={handleUsernameChange}
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                             />
                         </div>
                         </div>
                         <div>
                         <div class="flex items-center justify-between">
-                            <label for="" class="text-base font-medium text-gray-900">
+                            <label
+                            for="password"
+                            class="text-base font-medium text-gray-900"
+                            >
                             {" "}
                             Password{" "}
                             </label>
-                            <a
-                            href="#"
-                            title=""
-                            class="text-sm font-semibold text-black hover:underline"
-                            >
-                            {" "}
-                            Forgot password?{" "}
-                            </a>
                         </div>
                         <div class="mt-2">
                             <input
                             class="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
                             type="password"
                             placeholder="Password"
+                            id="password"
                             value={password}
-                            onChange={handlePasswordChange}
+                            onChange={(e) => setPassword(e.target.value)}
                             />
                         </div>
                         </div>
@@ -84,9 +89,8 @@ const LoginPage = () => {
                         <button
                             type="button"
                             class="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
-                            onClick={handleSubmit}
                         >
-                            Get started{" "}
+                            Create Account{" "}
                             <svg
                             xmlns="http://www.w3.org/2000/svg"
                             width="16"
@@ -121,7 +125,7 @@ const LoginPage = () => {
                             <path d="M20.283 10.356h-8.327v3.451h4.792c-.446 2.193-2.313 3.453-4.792 3.453a5.27 5.27 0 0 1-5.279-5.28 5.27 5.27 0 0 1 5.279-5.279c1.259 0 2.397.447 3.29 1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233a8.908 8.908 0 0 0-8.934 8.934 8.907 8.907 0 0 0 8.934 8.934c4.467 0 8.529-3.249 8.529-8.934 0-.528-.081-1.097-.202-1.625z"></path>
                         </svg>
                         </span>
-                        Sign in with Google
+                        Sign up with Google
                     </button>
                     </div>
                 </div>
@@ -136,8 +140,7 @@ const LoginPage = () => {
             </div>
         </section>
 
-
     );
 };
 
-export default LoginPage;
+export default SignUpPage;
